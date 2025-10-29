d079e5fb4147eae0ac3bcb2b7b627c314359769d
Amirhossein Rostami <amirhossein7rostami@gmail.com>
2025-10-29 16:11:19 +0100

    solve the error with the importing

---
+++ README.md
@@ -0,0 +1,21 @@
+# End to End Data scinece Project 
+
+### Wrokflows -- Ml Piplinie 
+ 
+
+ 1. Data Ingestion
+ 2. Data Transformation
+ 3. Model Trainer 
+ 4. Model Evaluation 
+
+## Workflows 
+
+1. Update config.yaml
+2. update Shcema.yaml
+3. Update Params.yaml
+4. Update the entity
+5. update the configuration manager in nsrc config
+6. update the components
+7. update the pipeline
+8. update the main.py
+
---
+++ src/datascience/utils/common.py
@@ -0,0 +1,92 @@
+import os 
+import yaml
+from src.datascience import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+from box.exceptions import BoxValueError, BoxKeyError
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads a yaml file and returns a Box object
+
+    Args:
+        path_to_yaml (Path): Path to the yaml file
+
+    Returns:
+        Box: Box object containing the yaml data
+    """
+    try:
+        with open(path_to_yaml, 'r') as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError as e:
+        logger.error(f"Error reading YAML file: {path_to_yaml} - {e}")
+        raise e
+    
+@ensure_annotations
+def save_json(path: Path, data: Any) -> None:
+    """Saves data to a json file
+
+    Args:
+        path (Path): Path to the json file
+        data (Any): Data to be saved
+    """
+    try:
+        with open(path, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+            logger.info(f"Data successfully saved to JSON file: {path}")
+    except Exception as e:
+        logger.error(f"Error saving data to JSON file: {path} - {e}")
+        raise e
+    
+@ensure_annotations
+def load_json(path: Path) -> Any:
+    """Loads data from a json file
+
+    Args:
+        path (Path): Path to the json file
+
+    Returns:
+        Any: Data loaded from the json file
+    """
+    try:
+        with open(path, 'r') as json_file:
+            data = json.load(json_file)
+            logger.info(f"Data successfully loaded from JSON file: {path}")
+            return data
+    except Exception as e:
+        logger.error(f"Error loading data from JSON file: {path} - {e}")
+        raise e
+    
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True) -> None:
+    """Creates directories if they do not exist
+
+    Args:
+        path_to_directories (list): List of directory paths to be created
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        logger.info(f"Directory created at: {path}")
+        
+@ensure_annotations
+def save_bin(data:Any, path:Path) -> None:
+    """Saves data to a binary file using joblib
+
+    Args:
+        data (Any): Data to be saved
+        path (Path): Path to the binary file
+    """
+    try:
+        joblib.dump(data, path)
+        logger.info(f"Data successfully saved to binary file: {path}")
+    except Exception as e:
+        logger.error(f"Error saving data to binary file: {path} - {e}")
+        raise e
+    
